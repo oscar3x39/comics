0e3294476bf33c1c14c1b241741130e26b7ed7f9
oscarlee <oscar3x39@gmail.com>
2021-02-05 10:43:20 +0800

    update

--- comicbus/source.js
+++ comicbus/source.js
@@ -3,7 +3,9 @@ var p = 1;
 var nums = parseInt($("#pageindex option:last-child")[0].value) - 1;
 
 function getImg() {
-	return ge(y673081s12(6) + y673081s12(5)).src = unescape(kn7k09xwgb + kn7k09xwgb + y673081s12(4) + sm3yb73_(n5q_8b, 0, 1) + q__7_2n7 + qxwgb7i1 + y673081s12(3) + y673081s12(2) + y673081s12(3) + kn7k09xwgb + sm3yb73_(n5q_8b, 1, 1) + kn7k09xwgb + ti + kn7k09xwgb + xtwd0c5q_8 + kn7k09xwgb + nn(p) + k1s1224_58 + sm3yb73_(e_x16b3c, mm(p), 3) + q__7_2n7 + y673081s12(1));
+	var text = $('script')[16].text
+	text = text.match(/ge\(.*?;/g)[1];
+	return eval(text)
 }
 
 var imgs = [];
